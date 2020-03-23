5791e142fadae81599f03d1102c24416074da733
Микола Мантицький <slwpke447@gmail.com>
2020-03-23 11:58:32 +0200

    add application context

---
+++ CovidBack/Enities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidBack.Enities
+{
+    [Table("tblProducts")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(200)]
+        public string Title { get; set; }
+
+        [Required, StringLength(1000)]
+        public string Price { get; set; }
+
+        [StringLength(1000)]
+        public string Url { get; set; }
+    }
+}
