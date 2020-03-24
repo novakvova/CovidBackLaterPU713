bf9a405fff656dc674ed96e03248e006f2b8dca7
novakvova <novakvova@gmail.com>
2020-03-24 10:27:17 +0200

    Get Data list products

---
+++ CovidBack/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CovidBack.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CovidBack.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        public IActionResult GetAll()
+        {
+            List<ProductDTO> model = new List<ProductDTO>() {
+
+                new ProductDTO{
+                    title = "Salo",
+                    price = "99999",
+                    url = "http://10.0.2.2/android/1.png"
+                }
+            };
+            return Ok(model);
+        }
+    }
+}
