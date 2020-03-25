446c88425a94e6d612a67b65c28f9305dceb830e
novakvova <novakvova@gmail.com>
2020-03-25 10:00:09 +0200

    Get list product

--- CovidBack/Controllers/ProductsController.cs
+++ CovidBack/Controllers/ProductsController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using CovidBack.DTO;
+using CovidBack.Enities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CovidBack.Controllers
 {
@@ -12,16 +15,34 @@ namespace CovidBack.Controllers
     [Produces("application/json")]
     public class ProductsController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+        private readonly IConfiguration _configuration;
+        public ProductsController(ApplicationDBContext context, 
+            IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
         public IActionResult GetAll()
         {
-            List<ProductDTO> model = new List<ProductDTO>() {
+            string domain = (string)_configuration.GetValue<string>("BackendDomain");
 
-                new ProductDTO{
-                    title = "Salo",
-                    price = "99999",
-                    url = "http://10.0.2.2/android/1.png"
-                }
-            };
+            var model = _context.Products
+                .Select(p=>new ProductDTO
+                {
+                    title=p.Title,
+                    price=p.Price,
+                    url= $"{domain}android/{p.Url}"
+                }).ToList();
+            Thread.Sleep(2000);
+            //List<ProductDTO> model = new List<ProductDTO>() {
+
+            //    new ProductDTO{
+            //        title = "Salo",
+            //        price = "99999",
+            //        url = "http://10.0.2.2/android/1.png"
+            //    }
+            //};
             return Ok(model);
         }
     }
