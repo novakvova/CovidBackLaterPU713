13570f89c5c67ff4355522638cf455564d3d32dc
Home <Home@DESKTOP-KA21DG9>
2020-03-30 09:30:14 +0300

    Add identity tabls

--- CovidBack/Enities/ApplicationDBContext.cs
+++ CovidBack/Enities/ApplicationDBContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +9,33 @@ using System.Threading.Tasks;
 
 namespace CovidBack.Enities
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<DbUser, DbRole, long, IdentityUserClaim<long>,
+                            DbUserRole, IdentityUserLogin<long>,
+                            IdentityRoleClaim<long>, IdentityUserToken<long>>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
         }
         public DbSet<Product> Products { get; set; }
-      
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<DbUserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+        }
+
     }
 }
